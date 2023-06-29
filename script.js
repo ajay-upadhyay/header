4b8cb357843f4dba4ab4b10160460542ba2d5272
coderajju <118973388+ajay-upadhyay@users.noreply.github.com>
2023-06-29 15:10:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+function my() {
+  let btn1 = document.getElementsByClassName("display1")[0];
+  btn1.style.display = "block";
+}
+function my2() {
+  let btn1 = document.getElementsByClassName("display1")[0];
+  btn1.style.display = "none";
+}
+function my3() {
+  let btn1 = document.getElementsByClassName("display2")[0];
+  btn1.style.display = "none";
+}
+function my4() {
+  let btn1 = document.getElementsByClassName("display2")[0];
+  btn1.style.display = "block";
+}
+function my5() {
+  let btn1 = document.getElementsByClassName("display3")[0];
+  btn1.style.display = "none";
+}
+function my6() {
+  let btn1 = document.getElementsByClassName("display3")[0];
+  btn1.style.display = "block";
+}
+function my7() {
+  let btn1 = document.getElementsByClassName("display4")[0];
+  btn1.style.display = "none";
+}
+function my8() {
+  let btn1 = document.getElementsByClassName("display4")[0];
+  btn1.style.display = "block";
+}
+function my9() {
+  let btn1 = document.getElementsByClassName("display5")[0];
+  btn1.style.display = "none";
+}
+function my10() {
+  let btn1 = document.getElementsByClassName("display5")[0];
+  btn1.style.display = "block";
+}
+function my11() {
+  let btn1 = document.getElementsByClassName("display6")[0];
+  btn1.style.display = "none";
+}
+function my12() {
+  let btn1 = document.getElementsByClassName("display6")[0];
+  btn1.style.display = "block";
+}
+
+// menu setting
+function hide() {
+  let bar = document.getElementsByClassName("side-bar")[0];
+  if (bar.style.display === "none") {
+    bar.style.display = "block";
+  } else {
+    bar.style.display = "none";
+  }
+}
